8e0d38e5efd9069835465ffb61d3bdfb664514f5
Joaron4 <82114258+Joaron4@users.noreply.github.com>
2022-10-17 22:28:53 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from otter.api import grade_submission
 grade = grade_submission("demo.ipynb", "autograder.zip")
 def test_hola():
     assert grade.get_score('q_1') == 1
-print('--->',(grade.get_score('q_1')))
+
